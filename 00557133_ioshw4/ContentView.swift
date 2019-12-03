15b96e38aa271aa2a2645477ce093c75aa855217
dejes <jack940197@gmail.com>
2019-12-03 19:40:14 +0800

    Initial Commit

---
+++ 00557133_ioshw4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  00557133_ioshw4
+//
+//  Created by User10 on 2019/12/3.
+//  Copyright © 2019 Starmy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
